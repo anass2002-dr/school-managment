e81e066d076ab64489d4b9dd48ea768c8f372b36
anass dermaj <anassdermaj163@gmail.com>
2024-01-15 17:47:42 +0100

    15-01-2024-18:47

--- src/app/components/tables/simpleTable.tsx
+++ src/app/components/tables/simpleTable.tsx
@@ -1,7 +1,28 @@
-export default function SimpleTable(){
+export default function SimpleTable({thead,data}:any){
+    const v_thead:string[]=thead
+    const v_data:string[]=data
     return(
         <>
-            
+        <table className="w-full text-sm text-left rtl:text-right bg-white text-gray-500 p-3">
+            <thead className="text-xs text-gray-700  uppercase rounded-lg bg-gray-100">
+                <tr>
+                    {
+                    v_thead.map((th)=>
+                        (
+                            <th scope="col" className="px-6 py-3">
+                            {th}
+                        </th>
+                        )
+                    )
+                    }
+                   
+                </tr>
+            </thead>
+            <tbody className="uppercase">
+                
+                
+            </tbody>
+        </table>
         </>
     )
 }
